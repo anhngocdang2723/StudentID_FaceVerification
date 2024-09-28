2ec2c6331394afbc5a865e4493c7978a3ed67ad3
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-09-28 23:28:46 +0700

    add comment

--- IDCard_processed.py
+++ IDCard_processed.py
@@ -4,11 +4,11 @@ import cv2
 img_path = r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\img\ThaiTuanIDCard.jpg"
 img = cv2.imread(img_path)
 
-# Chuyển ảnh sang dạng grayscale (màu xám)
+#chuyển ảnh xám
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Áp dụng ngưỡng hóa để loại bỏ nhiễu
+#áp dụng ngưỡng hoá để tạo ảnh nhị phân (ảnh chỉ chứa 2 màu đen và trắng) 
 _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
-# Lưu ảnh đã xử lý lại để kiểm tra
+#lưu ảnh đã xử lý
 cv2.imwrite(r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\img\ThaiTuanIDCard_processed.jpg", thresh)
