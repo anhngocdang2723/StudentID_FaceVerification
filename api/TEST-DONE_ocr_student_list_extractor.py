d01f156723a056807fa0dc22dde14585a932d1b4
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-10-04 19:13:11 +0700

    update comment và file path

--- api/TEST-DONE_ocr_student_list_extractor.py
+++ api/TEST-DONE_ocr_student_list_extractor.py
@@ -5,38 +5,39 @@ from unidecode import unidecode  #thư viện để in hoa bỏ dấu
 
 ocr = PaddleOCR(use_angle_cls=True, lang='vi')  # Đặt ngôn ngữ là tiếng Việt
 
-img_path = r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\0.png"
-excel_path = r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\DemoDanhSach.xlsx'
+#####thay thế hàm đọc ảnh bằng đọc từ excel
+#img_path = r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\0.png"
+excel_path = r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\api\List of candidates\DemoDanhSach.xlsx'
 
 ###### Sẽ thay thế bằng hàm đọc Excel
 #hàm đọc ảnh
-def read_from_image(img_path):
-    if os.path.exists(img_path):
-        # Nhận diện văn bản từ file ảnh
-        result = ocr.ocr(img_path, cls=True)
+# def read_from_image(img_path):
+#     if os.path.exists(img_path):
+#         # Nhận diện văn bản từ file ảnh
+#         result = ocr.ocr(img_path, cls=True)
 
-        # Trích xuất văn bản từ kết quả OCR
-        extracted_names = []
-        names_and_ids = []
+#         # Trích xuất văn bản từ kết quả OCR
+#         extracted_names = []
+#         names_and_ids = []
 
-        for line in result[0]:  # Xử lý từng dòng được OCR nhận diện
-            text = line[1][0].strip()
-            names_and_ids.append(text)
+#         for line in result[0]:  # Xử lý từng dòng được OCR nhận diện
+#             text = line[1][0].strip()
+#             names_and_ids.append(text)
 
-        # Xử lý để lấy tên và mã sinh viên
-        for i in range(1, len(names_and_ids)):  # Bắt đầu từ dòng thứ 2
-            current_line = names_and_ids[i]
-            if len(current_line) == 15 and current_line.isdigit():  # Nếu dòng hiện tại là mã sinh viên
-                student_id = current_line
-                student_name = names_and_ids[i - 1]  # Lấy tên từ dòng trên
-                # Loại bỏ dấu và chuyển thành in hoa
-                student_name = unidecode(student_name).upper()
-                extracted_names.append(f"{student_name} - {student_id}")
+#         # Xử lý để lấy tên và mã sinh viên
+#         for i in range(1, len(names_and_ids)):  # Bắt đầu từ dòng thứ 2
+#             current_line = names_and_ids[i]
+#             if len(current_line) == 15 and current_line.isdigit():  # Nếu dòng hiện tại là mã sinh viên
+#                 student_id = current_line
+#                 student_name = names_and_ids[i - 1]  # Lấy tên từ dòng trên
+#                 # Loại bỏ dấu và chuyển thành in hoa
+#                 student_name = unidecode(student_name).upper()
+#                 extracted_names.append(f"{student_name} - {student_id}")
 
-        return extracted_names
-    else:
-        print(f"File {img_path} không tồn tại.")
-        return []
+#         return extracted_names
+#     else:
+#         print(f"File {img_path} không tồn tại.")
+#         return []
 
 # Hàm phát hiện các cột chứa "Họ tên" và "Mã sinh viên"
 def find_columns(sheet):
@@ -89,22 +90,23 @@ def read_from_excel(excel_path):
         print(f"File Excel {excel_path} không tồn tại.")
         return []
 
+##### bỏ phần lấy từ ảnh
 # Đọc danh sách từ ảnh và Excel
-students_from_image = read_from_image(img_path)
+#students_from_image = read_from_image(img_path)
 students_from_excel = read_from_excel(excel_path)
 
 ########Thay thế bằng đọc qua Excel và lưu vào DB
 #hiện kết quả đọc ảnh
-if students_from_image:
-    print("Danh sách sinh viên từ ảnh:")
-    for student in students_from_image:
-        print(student)
+# if students_from_image:
+#     print("Danh sách sinh viên từ ảnh:")
+#     for student in students_from_image:
+#         print(student)
 
-    #lưu vào file txt
-    with open(r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\extracted_list\student_list_from_image.txt', 'w', encoding='utf-8') as f:
-        for student in students_from_image:
-            f.write(student + '\n')
-    print("Danh sách sinh viên từ ảnh đã được lưu vào file 'student_list_from_image.txt'.")
+#     #lưu vào file txt
+#     with open(r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\extracted_list\student_list_from_image.txt', 'w', encoding='utf-8') as f:
+#         for student in students_from_image:
+#             f.write(student + '\n')
+#     print("Danh sách sinh viên từ ảnh đã được lưu vào file 'student_list_from_image.txt'.")
 
 #hiển thị kết quả và lưu danh sách sinh viên từ Excel
 if students_from_excel:
@@ -114,7 +116,7 @@ if students_from_excel:
 
     ##### Sẽ thay thế bằng cách lưu vào DB
     #lưu sang txt
-    with open(r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\List of candidates\extracted_list\student_list_from_excel.txt', 'w', encoding='utf-8') as f:
+    with open(r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\api\List of candidates\extracted_list\student_list_from_excel.txt', 'w', encoding='utf-8') as f:
         for student in students_from_excel:
             f.write(student + '\n')
     print("Danh sách sinh viên từ Excel đã được lưu vào file 'student_list_from_excel.txt'.")
