c840be333451ed596fbba5956b81a5a3a54e1aca
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-10-11 19:49:04 +0700

    clean code read excel

--- api/excel_student_reader.py
+++ api/excel_student_reader.py
@@ -1,33 +1,27 @@
 from paddleocr import PaddleOCR
 import os
-import openpyxl  # thư viện đọc excel
-from unidecode import unidecode  # thư viện để in hoa bỏ dấu
+import openpyxl
+from unidecode import unidecode
 
 ocr = PaddleOCR(use_angle_cls=True, lang='vi')
 
 excel_path = r'D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\api\List of candidates\DemoDanhSach.xlsx'
 
-# hàm tìm cột chứa "họ tên" và "MSV"
 def find_columns(sheet):
     name_col = None
     msv_col = None
 
-    # duyệt qua các dòng để tìm dòng tiêu đề
     for row in sheet.iter_rows(values_only=True):
         for idx, cell_value in enumerate(row):
             if cell_value:
-                # tìm cột chứa "họ tên"
                 if "họ tên" in str(cell_value).lower():
-                    name_col = idx + 1  # openpyxl đánh số cột từ 1
-                # tìm cột chứa "msv"
+                    name_col = idx + 1
                 elif "mã sinh viên" in str(cell_value).lower():
                     msv_col = idx + 1                
-                # dừng lặp khi tìm thấy
                 if name_col and msv_col:
                     return name_col, msv_col
     return name_col, msv_col
 
-# hàm đọc danh sách sinh viên từ Excel
 def read_from_excel(excel_path):
     if os.path.exists(excel_path):
         wb = openpyxl.load_workbook(excel_path)
@@ -41,7 +35,6 @@ def read_from_excel(excel_path):
                 name = row[name_col - 1]
                 msv = row[msv_col - 1]
                 if name and msv: 
-                    # loại bỏ dấu, in hoa
                     name = unidecode(name).upper()
                     excel_data.append(f"{name} - {msv}")
             return excel_data
@@ -52,7 +45,6 @@ def read_from_excel(excel_path):
         print(f"File Excel {excel_path} không tồn tại.")
         return []
 
-# gọi hàm đọc từ Excel
 students_from_excel = read_from_excel(excel_path)
 
 ######## Sẽ cập nhật lưu kết quả đọc được từ file excel vào DB sau ########
