c2a659657d5e4fcf6ec97ff5311b99160e5dda2d
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-10-13 10:50:06 +0700

    test yolov11

--- test/tempCodeRunnerFile.py
+++ test/tempCodeRunnerFile.py
@@ -1,61 +1,30 @@
 import cv2
-import numpy as np
-from paddleocr import PaddleOCR
+from ultralytics import YOLO
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
+# tải model YOLOv11s
+model = YOLO('yolo11n.pt')  # Thay thế bằng 'yolo11n.pt' nếu có mô hình phù hợp
 
-img_path = r"D:\\Edu\\Python\\StudentID_FaceVerification\\student-id-face-matching\\test\\imgTest\\NgocAnhIDCard.jpg"
-img = cv2.imread(img_path)
+cap = cv2.VideoCapture(r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\test\imgTest\video.mp4")
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if not cap.isOpened():
+    print("Không thể mở video")
+    exit()
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Không thể đọc khung hình, hoặc hết video")
+        break
 
-# giảm kích thước ảnh
-scale_percent = 50
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-resized_img = cv2.resize(img, dim)
+    results = model(frame)
 
-faces = face_cascade.detectMultiScale(cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, minNeighbors=5)
+    annotated_frame = results[0].plot()
 
-if len(faces) > 0:
-    (x, y, w, h) = faces[0]
-    
-    # chuyển toạ độ theo kích thước gốc
-    x = int(x * (100 / scale_percent))
-    y = int(y * (100 / scale_percent))
-    w = int(w * (100 / scale_percent))
-    h = int(h * (100 / scale_percent))
+    cv2.imshow('YOLOv8 Video Detection', annotated_frame)
 
-    face_img = img[y:y+h, x:x+w]
-    cv2.imwrite(r"D:\\Edu\\Python\\StudentID_FaceVerification\\student-id-face-matching\\test\\resultTest\\NgocAnhIDCard_face.jpg", face_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Dừng video")
+        break
 
-    cv2.imshow("Face", face_img)
-
-    # ap dụng ngưỡng hóa để tạo ảnh nhị phân
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-
-    # Chạy OCR trên ảnh ngưỡng hóa
-    result = ocr.ocr(thresh, cls=True)
-
-    # Vẽ box lên ảnh ngưỡng hóa
-    for line in result:
-        for word_info in line:
-            # Lấy toạ độ các góc của box
-            points = word_info[0]
-            # Chuyển đổi toạ độ sang dạng integer
-            points = [(int(point[0]), int(point[1])) for point in points]
-            # Vẽ viền theo points
-            cv2.polylines(thresh, [np.array(points)], isClosed=True, color=(0, 255, 0), thickness=2)
-
-    cv2.imwrite(r'D:\\Edu\\Python\\StudentID_FaceVerification\\student-id-face-matching\\test\\resultTest\\NgocAnhIDCard_processed_with_boxes.jpg', thresh)
-
-    cv2.imshow('Processed Image with OCR boxes', thresh)
-
-else:
-    print("Không tìm thấy khuôn mặt.")
-
-cv2.waitKey(0)
+cap.release()
 cv2.destroyAllWindows()
