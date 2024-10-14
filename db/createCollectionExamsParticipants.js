56b85d395a85ba22572fc899373f28fdbd89fc3f
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-10-14 09:21:41 +0700

    update main

--- db/createCollectionExamsParticipants.js
+++ db/createCollectionExamsParticipants.js
@@ -1,60 +1,103 @@
-db.createCollection("Exams")
-db.Exams.insertMany([
-    {
-        "exam_id": "INF30033",
-        "exam_name": "An toàn thông tin",
-        "exam_date": "2024-12-01",
-        "rooms": [
-            {
-                "exam_room": "TTKT304",
-                "invigilator_id": "GT101"
-            },
-            {
-                "exam_room": "TTKT305",
-                "invigilator_id": "GT103"
-            },
-            {
-                "exam_room": "TTKT306",
-                "invigilator_id": "GT105"
-            }
-        ]
-    },
-    {
-        "exam_id": "INF20141",
-        "exam_name": "Thị giác máy tính",
-        "exam_date": "2024-12-03",
-        "rooms": [
-            {
-                "exam_room": "TTKT404",
-                "invigilator_id": "GT102"
-            },
-            {
-                "exam_room": "TTKT405",
-                "invigilator_id": "GT104"
-            },
-            {
-                "exam_room": "TTKT406",
-                "invigilator_id": "GT106"
-            }
-        ]
-    },
-    {
-        "exam_id": "INF20007",
-        "exam_name": "Trí tuệ nhân tạo",
-        "exam_date": "2024-12-05",
-        "rooms": [
-            {
-                "exam_room": "TTKT504",
-                "invigilator_id": "GT107"
-            },
-            {
-                "exam_room": "TTKT505",
-                "invigilator_id": "GT108"
-            },
-            {
-                "exam_room": "TTKT506",
-                "invigilator_id": "GT109"
-            }
-        ]
-    }
+db.createCollection("ExamParticipants")
+db.ExamParticipants.insertMany([
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110001",
+    "exam_room": "TTKT304"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110002",
+    "exam_room": "TTKT304"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110003",
+    "exam_room": "TTKT305"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110004",
+    "exam_room": "TTKT305"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110005",
+    "exam_room": "TTKT306"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110006",
+    "exam_room": "TTKT306"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110007",
+    "exam_room": "TTKT306"
+  },
+  {
+    "exam_id": "INF30033",
+    "std_id": "215748020110008",
+    "exam_room": "TTKT306"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110009",
+    "exam_room": "TTKT404"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110010",
+    "exam_room": "TTKT404"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110011",
+    "exam_room": "TTKT405"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110012",
+    "exam_room": "TTKT405"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110013",
+    "exam_room": "TTKT406"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110014",
+    "exam_room": "TTKT406"
+  },
+  {
+    "exam_id": "INF20141",
+    "std_id": "215748020110015",
+    "exam_room": "TTKT406"
+  },
+  {
+    "exam_id": "INF20007",
+    "std_id": "215748020110016",
+    "exam_room": "TTKT504"
+  },
+  {
+    "exam_id": "INF20007",
+    "std_id": "215748020110017",
+    "exam_room": "TTKT504"
+  },
+  {
+    "exam_id": "INF20007",
+    "std_id": "215748020110018",
+    "exam_room": "TTKT505"
+  },
+  {
+    "exam_id": "INF20007",
+    "std_id": "215748020110019",
+    "exam_room": "TTKT505"
+  },
+  {
+    "exam_id": "INF20007",
+    "std_id": "215748020110020",
+    "exam_room": "TTKT506"
+  }
 ])
