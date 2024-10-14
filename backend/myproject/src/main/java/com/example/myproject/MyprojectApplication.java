15adbf338289dfd0548b10aefc962a83af86d001
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-10-14 07:59:19 +0700

    build lại backend

--- backend/myproject/src/main/java/com/example/myproject/MyprojectApplication.java
+++ backend/myproject/src/main/java/com/example/myproject/MyprojectApplication.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package com.example.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MyprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MyprojectApplication.class, args);
 	}
 
 }
-
--- backend/myproject/src/test/java/com/example/myproject/MyprojectApplicationTests.java
+++ backend/myproject/src/test/java/com/example/myproject/MyprojectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.myproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MyprojectApplicationTests {
 
 	@Test
 	void contextLoads() {
