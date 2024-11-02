3baf95806d5c4b1e332294c35f6be25b50f9c222
AnhNgocDang <111883222+anhngocdang2723@users.noreply.github.com>
2024-11-03 02:01:24 +0700

    T1

--- camera_detection.py
+++ camera_detection.py
@@ -4,19 +4,16 @@ from fastapi import Response
 import cv2
 from ultralytics import YOLO
 
-# Load model YOLO đã train
 model = YOLO(r"D:\Edu\Python\StudentID_FaceVerification\student-id-face-matching\api\models\best.pt")  # Thay bằng đường dẫn đến model YOLO của bạn
 
 async def detect_card_from_camera():
-    # Mở camera
-    cap = cv2.VideoCapture(0)  # 0 là chỉ số camera, thay đổi nếu cần
+    cap = cv2.VideoCapture(0)
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Phát hiện thẻ sinh viên với YOLO
         results = model.predict(frame)
         for result in results:
             # Lấy toạ độ bounding box và vẽ lên khung hình
@@ -29,5 +26,4 @@ async def detect_card_from_camera():
         frame_data = buffer.tobytes()
         yield Response(content=frame_data, media_type="image/jpeg")
         
-    # Khi hoàn thành, giải phóng tài nguyên
     cap.release()
